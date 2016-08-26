9d9198fade4f9acab1dfd3876b65f469c2afc00d
hidehalo <tianchen_cc@yeah.net>
2016-08-26 15:08:58 +0800

    ok

--- src/test.php
+++ src/test.php
@@ -1,7 +1,7 @@
 <?php
 require_once "EmojiDetector.php";
 $ed = new EmojiDetector();
-$text = "大王派我来查Emoji:🌒🌏🍃!?1234567890-=+\\|/?\\";
+$text = "大王派我来查Emoji:🌒🌏🍃🇬🇧!?1234567890-=+\\|/?\\";
 var_dump($text);
 print '<br/>';
 $format = "ncrdec";
