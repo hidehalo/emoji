09e09f312c98c61cf0904b7e67fba93804481601
halobangbangboom <tianchen_cc@yeah.net>
2016-09-28 23:13:32 +0800

    !!!

--- src/Adapter/HtmlAdapter.php
+++ src/Adapter/HtmlAdapter.php
@@ -5,39 +5,33 @@ class HtmlAdpater extends Adapter
     protected $pattern;
     public function __construct()
     {
-//        http://apps.timwhitlock.info/emoji/tables/unicode
-//        1.Emoticons( 1F601 - 1F64F )
-//        2. Dingbats ( 2702 - 27B0 )
-//        3. Transport and map symbols ( 1F680 - 1F6C0 )
-//        4. Enclosed characters ( 24C2 - 1F251 )
-//        5. Uncategorized
-//        6a. Additional emoticons ( 1F600 - 1F636 )
-//        6b. Additional transport and map symbols ( 1F681 - 1F6C5 )
-//        6c. Other additional symbols ( 1F30D - 1F567 )
+        //http://apps.timwhitlock.info/emoji/tables/unicode
         $this->pattern ='/['.
-            $this->unichr(0x1F600).'-'.$this->unichr(0x1F64F).
-            $this->unichr(0x2700).'-'.$this->unichr(0x27BF).
-            $this->unichr(0x1F680).'-'.$this->unichr(0x1F6FF).
+            $this->unichr(0x0080).'-'.$this->unichr(0x02AF).
+            $this->unichr(0x0370).'-'.$this->unichr(0x03FF).
+            $this->unichr(0x2000).'-'.$this->unichr(0x206F).
+            $this->unichr(0x20D0).'-'.$this->unichr(0x214F).
+            $this->unichr(0x2190).'-'.$this->unichr(0x23FF).
             $this->unichr(0x2460).'-'.$this->unichr(0x1F251).
-            $this->unichr(0x0001).'-'.$this->unichr(0x007F).
-            $this->unichr(0x20D0).'-'.$this->unichr(0x20FF).
-            $this->unichr(0x2100).'-'.$this->unichr(0x214F).
-            $this->unichr(0x2300).'-'.$this->unichr(0x23FF).
             $this->unichr(0x25A0).'-'.$this->unichr(0x25FF).
-            $this->unichr(0x2600).'-'.$this->unichr(0x26FF).
-            $this->unichr(0x2900).'-'.$this->unichr(0x297F).
+            $this->unichr(0x2600).'-'.$this->unichr(0x27EF).
+            $this->unichr(0x2900).'-'.$this->unichr(0x29FF).
             $this->unichr(0x2B00).'-'.$this->unichr(0x2BFF).
+            $this->unichr(0x2C60).'-'.$this->unichr(0x2C7F).
             $this->unichr(0x3000).'-'.$this->unichr(0x303F).
             $this->unichr(0x1F000).'-'.$this->unichr(0x1F02F).
-            $this->unichr(0x1F300).'-'.$this->unichr(0x1F5FF).
-            $this->unichr(0x0080).'-'.$this->unichr(0x00FF).
-            $this->unichr(0x2000).'-'.$this->unichr(0x206F).
+            $this->unichr(0x1F0A0).'-'.$this->unichr(0x1F0FF).
             $this->unichr(0x1F100).'-'.$this->unichr(0x1F1FF).
             $this->unichr(0x1F300).'-'.$this->unichr(0x1F5FF).
-            $this->unichr(0xE000).'-'.$this->unichr(0xF8FF).
+            $this->unichr(0x1F30D).'-'.$this->unichr(0x1F567).
             $this->unichr(0x1F600).'-'.$this->unichr(0x1F64F).
             $this->unichr(0x1F680).'-'.$this->unichr(0x1F6FF).
-            $this->unichr(0x1F30D).'-'.$this->unichr(0x1F567).
+            $this->unichr(0x1F910).'-'.$this->unichr(0x1F918).
+            $this->unichr(0x1F980).'-'.$this->unichr(0x1F9C0).
+            $this->unichr(0x1E00).'-'.$this->unichr(0x1EFF).
+            $this->unichr(0xFE00).'-'.$this->unichr(0xFE0F).
+            $this->unichr(0xFE30).'-'.$this->unichr(0xFE4F).
+            $this->unichr(0xE000).'-'.$this->unichr(0xF8FF).
         ']/u';
     }
     // source - http://php.net/manual/en/function.ord.php#109812
--- src/test.php
+++ src/test.php
@@ -4,13 +4,15 @@ require_once "demo.php";
 use Hidehalo\Emoji;
 $emoji = new Emoji();
 echo '
-<form method="post">
-    <input type="text" name="text">
-    <button type="submit">提交</button>
+<form method="post" style="text-align:center;">
+    <textarea type="text" name="text" rows="5" cols="120"></textarea>
+    <br>
+    <button type="submit" style="width:864px;">提交</button>
 </form>
 ';
-$string = @$_REQUEST['text'];
-if($string)
-    echo "<p>";
-    print_r($emoji->replace($string,'dec'));
+$string = @$_REQUEST['text']?$_REQUEST['text']:"";
+if($string) {
+    echo "<p style=\"text-align:center\">";
+    print_r("没有匹配到的:".$emoji->replace($string,'clean'));
     echo "</p>";
+}
